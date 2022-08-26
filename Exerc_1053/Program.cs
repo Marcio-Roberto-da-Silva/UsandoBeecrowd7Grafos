b282964b7783d8da7e674695548b12438eaea69f
Marcio Roberto <marcio.silva@keeggo.com>
2022-08-26 10:11:18 -0300

    Grafos no CSharp

---
+++ Exerc_1053/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_1053 {
+    class Program {
+        static void Main(string[] args) {
+
+            int t= int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < t; i++) {
+
+                int n = int.Parse(Console.ReadLine());
+
+                for( int e = 0; e < n; e++) {
+
+                    double A1 = 1;
+                    double A1 = 1;
+                    double A1 = 1;
+
+                }
+                string[] vet = Console.ReadLine().Split(' ');
+
+                X = double.Parse(vet[0]);
+                Y = double.Parse(vet[1]);
+
+                
+
+
+                
+
+                if (X >= 0 && X <= 5) {
+
+                        
+
+                }
+
+                }
+           
+            } 
+        }
+    }
+}
---
+++ Exerc_1053/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Exerc_1053")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exerc_1053")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("8cd1c0b3-b0e1-41b7-a3c9-b6fb99e2ca2d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
