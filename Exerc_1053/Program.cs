3bcc01838238bcd858e9ddbd55f1ba5e0c71a0f2
Marcio Roberto <marcio.silva@keeggo.com>
2022-08-30 10:42:42 -0300

    atualizando meu código

--- Exerc_1053/Program.cs
+++ Exerc_1053/Program.cs
@@ -5,39 +5,37 @@ namespace Exerc_1053 {
     class Program {
         static void Main(string[] args) {
 
-            int t= int.Parse(Console.ReadLine());
-            
+            int t = int.Parse(Console.ReadLine());
+
 
             for (int i = 0; i < t; i++) {
 
                 int n = int.Parse(Console.ReadLine());
 
-                for( int e = 0; e < n; e++) {
+                for (int e = 0; e < n; e++) {
 
-                    double A1 = 1;
-                    double A1 = 1;
-                    double A1 = 1;
+                   
+                    string[] vet = Console.ReadLine().Split(' ');
 
+                   string X = vet[0];
+                   string Y = vet[1];
+                    string[] coluna = new string[5];
+                    int[] linha = new int[5] { 1, 2, 3, 4, 5 };
+                    
+                    coluna[0]= "A";
+                    coluna[1]= "B";
+                    coluna[2]= "C";
+                    coluna[3]= "D";
+                    coluna[4]= "E";
+
+
+                    if (i+t *1 == 1) {
+                        Console.ReadLine();
+                        Console.WriteLine("Case 1:" + coluna + linha ) ;
+                    }
                 }
-                string[] vet = Console.ReadLine().Split(' ');
 
-                X = double.Parse(vet[0]);
-                Y = double.Parse(vet[1]);
-
-                
-
-
-                
-
-                if (X >= 0 && X <= 5) {
-
-                        
-
-                }
-
-                }
-           
-            } 
+            }Console.ReadKey();
         }
     }
 }
